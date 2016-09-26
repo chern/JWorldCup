0cb6382d4aea03c1dec63787ad7684cebd1286ac
Nishan D'Souza <nishand@gmail.com>
2016-09-25 20:27:25 -0700

    Frameworks for reading/writing + keyevents

--- Engine.java
+++ Engine.java
@@ -1,14 +1,23 @@
 // import classes
 import javax.swing.JFrame;
-
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 // class Engine contains main method to be run
 public class Engine {
     // main method creates and interacts with instances of class SoccerGame
     public static void main(String[] args) {
        JFrame frame = new JFrame("Soccer Game");
        frame.setSize(500, 520);
-       SoccerGame game = new SoccerGame(500, 500);
+       SoccerGame game = new SoccerGame(500, 500, true);
        frame.add(game);
        frame.setVisible(true);
+       class KeyListen implements KeyListener {
+           public void keyPressed(KeyEvent e) {
+            }
+           public void keyTyped(KeyEvent e) {
+            } 
+           public void keyReleased(KeyEvent e) {
+            } 
+        }
     }
 }
--- SoccerGame.java
+++ SoccerGame.java
@@ -5,26 +5,27 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
-
 // class SoccerGame
 public class SoccerGame extends JPanel {
     public int width;
     public int height;
     public final Color darkGreen = new Color(20, 170, 20);
     public final Color lightGreen = new Color(40, 190, 40);
-    public SoccerGame(int w, int h) {
+    public boolean light;
+    public SoccerGame(int w, int h, boolean l) {
         this.width = w;
         this.height = h;
+        this.light = l;
         this.setBackground(darkGreen);
     }
 
     protected void paintComponent(Graphics g) {
-        // draw background
+        //*The Following is the drawing of the field*
+        //  background
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        // draw field grass
+        //  field grass
         final int rects = 5;
-        boolean light = false;
         for (int i = 0; i < rects; i++) {
             if (light) g.setColor(this.lightGreen);
             else g.setColor(this.darkGreen);
@@ -61,5 +62,11 @@ public class SoccerGame extends JPanel {
             int y2 = goalY + i * goalHeight/lines;
             g2.drawLine(x1, y1, x2, y2);
         }
+        //*drawing of field finished
+    }
+    
+    public void readFile() {
+    }
+    public void saveFile() {
     }
 }
