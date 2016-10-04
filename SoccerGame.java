288782b3f96656f439ef2897ef611af36133523f
Nishan D'Souza <nishand@gmail.com>
2016-10-04 15:24:39 -0700

    Added playerData.csv

--- SoccerGame.java
+++ SoccerGame.java
@@ -117,10 +117,12 @@ public class SoccerGame extends JComponent {
     }
     
     public void readFile() {
-
+        
     }
 
     public void saveFile() {
-
+        for(Player p : playerList) {
+            
+        }
     }
 }
